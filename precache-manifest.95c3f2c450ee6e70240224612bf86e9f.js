f4f654b98a421dfdd1ad9011c6f0e2e2b12bd015
Smelliott94 <marcuselliott94@gmail.com>
2019-12-23 00:49:47 +0100

    Updates

---
+++ precache-manifest.95c3f2c450ee6e70240224612bf86e9f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4cbc3b5b3f48c4fd8638a7c55dc72bc6",
+    "url": "/Wortschatz-React/index.html"
+  },
+  {
+    "revision": "515858aca06ff5fcc332",
+    "url": "/Wortschatz-React/static/css/main.10578d90.chunk.css"
+  },
+  {
+    "revision": "9e3b8b8d893ca52f8830",
+    "url": "/Wortschatz-React/static/js/2.f17b9a3d.chunk.js"
+  },
+  {
+    "revision": "85605b2cae435653bed9f6f28884786e",
+    "url": "/Wortschatz-React/static/js/2.f17b9a3d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "515858aca06ff5fcc332",
+    "url": "/Wortschatz-React/static/js/main.75475cd4.chunk.js"
+  },
+  {
+    "revision": "a68d672681053af56059",
+    "url": "/Wortschatz-React/static/js/runtime-main.366f76d4.js"
+  }
+]);
